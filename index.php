38801d3eb416714311b54c0e04497cdca57421a8
PrachiDeshmukh13 <prachi13.deshmukh@gmail.com>
2016-07-20 17:36:16 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+
+<frameset rows="30%,75%" border=no>
+<frame src='top.html' scrolling=yes>
+<frameset cols="25%,75%" border=no>
+<frame src='left.html'>
+<frame src='main.html' name='main'>
+</frameset>
+</frameset>
+
+</html>
